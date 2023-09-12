653b3dbb7c4dd7c5bfa34846de6a2cb012e74f8a
Hajongseon <jongsun3322@naver.com>
2023-09-13 08:57:10 +0900

    swiper 기능 사용한 슬라이드 요소 추가

--- js/main.js
+++ js/main.js
@@ -52,8 +52,42 @@ window.addEventListener('scroll', _.throttle(function() { // scroll 이벤트를
 
 const fadeELs = document.querySelectorAll('.visual .fade-in');
 fadeELs.forEach(function(fadeEl, index) {
+    // gsap.to(요소, 지속시간, 옵션);
     gsap.to(fadeEl, 1, {
         delay: (index + 1) * .7, // 0.7, 1.4, 2.1, 2.7
         opacity: 1
     });
+});
+
+
+
+// Swiper. js ---
+// new = 생성자 클래스, 소가로안에다 넣는걸 인수라함, {} = 객체 데이터형태
+
+// new Swiper(선택자,옵션 )
+new Swiper('.notice-line .swiper-container', {
+    direction: 'vertical', // 방향
+    autoplay: true,
+    loop: true //반복 재생 여부
+}); 
+
+
+// Swiper slide
+new Swiper('.promotion .swiper-container', {
+    // direction 기본값 (수평), 따로 명시할 필욘 없음 // 뒤에 slide(s) 주의해서 붙이기ㅠㅠ
+    slidesPerView: 3, // 한번에 보여줄 슬라이드 개수
+    spaceBetween: 10, // 슬라이드 사이 여백 (10px씩)
+    centeredSlides: true, // 1번 슬라이드가 가운데 보이기
+    loop: true,
+    autoplay: { //autoplay 객체 데이터로 명시할 수 있음
+        delay: 5000 // 5s ex) 300 = 0.3s
+    },
+    pagination: {
+        el: '.promotion .swiper-pagination', // 페이지 번호 요소 선택자
+        clickable: true // 사용자의 페이지 번호 요소 제어 가능 여부(눌러서 제어 가능한지)
+    },
+    navigation: {
+        prevEl: '.promotion .swiper-prev', // 이전 슬라이드 보는 버튼
+        nextEl: '.promotion .swiper-next' // 다음 슬라이드 보는 버튼
+    }
 });
