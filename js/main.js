fa834f7f129f1e2e4ba00a8c4a4320ab0480c5db
Hajongseon <jongsun3322@naver.com>
2023-09-14 15:07:46 +0900

    고정 배경 삽입

--- js/main.js
+++ js/main.js
@@ -107,3 +107,30 @@ promotionToggleBtn.addEventListener('click', function () {
         promotionEl.classList.remove('hide');
     }
 });
+
+
+// 범위 랜덤 함수(소수점 2자리까지)
+function random(min, max) {
+    // `.toFixed()`를 통해 반환된 문자 데이터를,
+    // `parseFloat()`을 통해 소수점을 가지는 숫자 데이터로 변환
+    return parseFloat((Math.random() * (max - min) + min).toFixed(2))
+  }
+
+// Youtube floating 애니메이션 적용
+function floatingObject(selector, delay, size) {
+    // gsap.to(요소, 지속시간, {옵션});
+    gsap.to(
+        selector, // 선택자
+        random(1.5, 2.5), // 애니메이션 동작 시간 
+        { // 옵션
+            y: size,
+            repeat: -1, // 무한반복 (몇번 반복할건지)
+            yoyo: true, // 한번 재생된 애니메이션을 다시 뒤로 재생해서 위에서 밑으로 내려왔다가 다시 위로 올라가는 구조를 만들어줌
+            ease: Power1.easeInOut,
+            delay: random(0, delay)
+        }
+    );
+}
+floatingObject('.floating1', 1, 15); // delay: 1(s), y: 15px
+floatingObject('.floating2', .5, 15); // delay: 0.5(S), y: 15px
+floatingObject('.floating3', 1.5, 20); // delay: 1.5(S), y: 20px
